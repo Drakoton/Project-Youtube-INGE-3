7dc8c88600a91dcbaefdf1aed19c28782eb4a7b8
Dorian Rakotonanahary <79524563+Drakoton@users.noreply.github.com>
2025-02-24 20:03:07 +0100

    Update sentiment.js

--- NextJS/API/sentiment.js
+++ NextJS/API/sentiment.js
@@ -3,10 +3,8 @@ import { exec } from 'child_process';
 export default function handler(req, res) {
   if (req.method === 'GET') {
     console.log("Exécution du script Python...");
-    
-    // Exécution du script Python
+
     exec('python C:/Users/PC/Downloads/Youtube/mon-projet/pages/api/sentiment_analysis.py', (error, stdout, stderr) => {
-      // Log des erreurs et de la sortie du script Python
       if (error) {
         console.error("Erreur d'exécution du script:", error);
         return res.status(500).json({ error: stderr || error.message });
@@ -19,11 +17,28 @@ export default function handler(req, res) {
 
       console.log("Sortie du script Python (stdout):", stdout);
 
-      // Tentative de parsing de la sortie du script Python en JSON
       try {
         const data = JSON.parse(stdout); // Parsing de stdout
         console.log("Données JSON reçues:", data);
-        return res.status(200).json(data); // Renvoi des données en JSON
+
+        // Utilisation des bonnes clés provenant du script Python
+        const sentimentCountsDesc = {
+          positif: data.description_sentiments['positif'] || 0,
+          neutre: data.description_sentiments['neutre'] || 0,
+          négatif: data.description_sentiments['négatif'] || 0,
+        };
+
+        const sentimentCountsComm = {
+          positif: data.comments_sentiments['positif'] || 0,
+          neutre: data.comments_sentiments['neutre'] || 0,
+          négatif: data.comments_sentiments['négatif'] || 0,
+        };
+
+        // Renvoi des données formatées pour le frontend
+        return res.status(200).json({
+          description_sentiments: sentimentCountsDesc,
+          comments_sentiments: sentimentCountsComm
+        });
       } catch (err) {
         console.error("Erreur de parsing JSON:", err);
         return res.status(500).json({ error: 'Erreur de formatage des données reçues' });
